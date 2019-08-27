aa3d8aad6f65caaa59e52a6b28114d085cab5c09
Jan Markiewicz <gurujasko11@gmail.com>
2019-08-27 21:04:05 +0200

    move repositories into model files

--- app/models/Category.scala
+++ app/models/Category.scala
@@ -1,6 +1,11 @@
 package models
 
+import javax.inject.{Inject, Singleton}
+import play.api.db.slick.DatabaseConfigProvider
 import play.api.libs.json._
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.{ExecutionContext, Future}
 
 case class Category(
                      category_id: Long,
@@ -8,4 +13,44 @@ case class Category(
 
 object Category {
   implicit val categoryFormat = Json.format[Category]
+}
+
+@Singleton
+class CategoryRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
+  val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  import dbConfig._
+  import profile.api._
+
+  class CategoryTable(tag: Tag) extends Table[Category](tag, "categories") {
+    def category_id = column[Long]("category_id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("category_name")
+    def * = (category_id, name) <> ((Category.apply _).tupled, Category.unapply)
+  }
+
+  val category = TableQuery[CategoryTable]
+
+  def create(name: String): Future[Category] = db.run {
+    (category.map(c => (c.name))
+      returning category.map(_.category_id)
+      into {case ((name), category_id) => Category(category_id, name)}
+      ) += (name)
+  }
+
+  def list(): Future[Seq[Category]] = db.run {
+    category.result
+  }
+
+  def delete(id: Long): Future[Unit] = db.run{
+    (category.filter(_.category_id === id).delete).map(_ => ())
+  }
+
+  def findById(id: Long): Future[scala.Option[Category]] = db.run{
+    category.filter(_.category_id === id).result.headOption
+  }
+
+  def update(newValue: Category) = db.run{
+    category.insertOrUpdate(newValue)
+  }
+
 }
--- app/models/Product.scala
+++ app/models/Product.scala
@@ -1,6 +1,11 @@
 package models
 
+import javax.inject.{Inject, Singleton}
+import play.api.db.slick.DatabaseConfigProvider
 import play.api.libs.json._
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.{ExecutionContext, Future}
 
 case class Product(
                     product_id: Long,
@@ -14,4 +19,48 @@ case class Product(
 
 object Product {
   implicit val productFormat = Json.format[Product]
+}
+
+
+@Singleton
+class ProductRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
+
+  val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  import dbConfig._
+  import profile.api._
+
+  class ProductTable(tag: Tag) extends Table[Product](tag, "products") {
+    def product_id = column[Long]("product_id", O.PrimaryKey, O.AutoInc)
+    def category_id = column[Long]("category_id")
+    def name = column[String]("name")
+    def description = column[String]("description")
+    def country_of_origin = column[String]("country_of_origin")
+    def weight = column[Int]("weight")
+    def price = column[Double]("price")
+
+    def * = (product_id, category_id, name, description, country_of_origin, weight, price) <> ((Product.apply _).tupled, Product.unapply)
+  }
+
+  val product = TableQuery[ProductTable]
+
+  def create(category_id: Long, name: String, description: String, country_of_origin: String, weight: Int, price: Double): Future[Product] = db.run {
+    (product.map(p => (p.category_id, p.name, p.description, p.country_of_origin, p.weight, p.price))
+      returning product.map(_.product_id)
+      into {case((category_id, name, description, country_of_origin, weight, price), product_id) =>
+      Product(product_id, category_id, name, description, country_of_origin, weight, price)}
+      ) += (category_id, name, description, country_of_origin, weight, price)
+  }
+
+  def list(): Future[Seq[Product]] = db.run {
+    product.result
+  }
+
+  def delete(id: Long): Future[Unit] = db.run {
+    (product.filter(_.product_id === id).delete).map(_ => ())
+  }
+
+  def findById(id: Long): Future[scala.Option[Product]] = db.run {
+    product.filter(_.product_id === id).result.headOption
+  }
 }
